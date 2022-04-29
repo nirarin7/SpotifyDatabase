3777b6a6fd81a9ea2b50d44e4f10c67cdacfa2a9
nirarin7 <estearin@isu.edu>
2022-04-29 17:42:45 -0600

    more heroku stuff

--- SpotifyStats/settings.py
+++ SpotifyStats/settings.py
@@ -10,7 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
+import django_heroku
 from pathlib import Path
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -148,3 +150,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 #     BASE_DIR / "static",
 #     '/var/www/static/',
 # ]
+django_heroku.settings(locals())
